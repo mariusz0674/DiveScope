9d8b0ec2c86b5d915b2d0eca80f8aff68b614186
mbartosik <mbartosik@consdata.com>
2025-11-02 18:15:14 +0100

    little stylowanko

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -7,9 +7,6 @@ document.getElementById('highlightElements').addEventListener('click', function(
 
 document.getElementById('mouseFreeze').addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'mouseFreeze:toggle' }, function (res) {
-            // (opcjonalnie) zaktualizuj etykietę przycisku na podstawie res.enabled
-            // document.getElementById('event-shield-toggle').textContent = res?.enabled ? 'Shield ON' : 'Shield OFF';
-        });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'mouseFreeze:toggle' });
     });
 });
